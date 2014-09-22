9a581e992cdc6b82c35fd60670418eab7cd63306
Febrian Doni <donikhun@gmail.com>
2014-09-22 15:54:44 +0700

    finish

--- SaveDoc/ViewController.m
+++ SaveDoc/ViewController.m
@@ -7,8 +7,8 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
+#import <MessageUI/MFMailComposeViewController.h>
+@interface ViewController () <MFMailComposeViewControllerDelegate>
 
 @end
 
@@ -18,6 +18,34 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+  NSMutableString *someText = [NSMutableString stringWithString:@"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"  \"http://www.w3.org/TR/html4/loose.dtd\"><html xmlns:v=\"urn:schemas-microsoft-com:vml\"xmlns:o=\"urn:schemas-microsoft-com:office:office\"xmlns:w=\"urn:schemas-microsoft-com:office:word\"xmlns=\"http://www.w3.org/TR/REC-html40\"><h2>Spectacular Mountains</h2>"];
+  NSString *fileURLString = [[[NSBundle mainBundle] URLForResource:@"pic_mountain" withExtension:@"jpg"] absoluteString];
+  [someText appendFormat:@"<img src=\"%@\" alt=\"Mountain View\" style=\"width:304px;height:228px\"/></html>", fileURLString];
+  NSString *destinationPath = [NSTemporaryDirectory() stringByAppendingPathComponent:@"MyFile.doc"];
+  
+  NSError *error = nil;
+  BOOL succeeded = [someText writeToFile:destinationPath
+                              atomically:YES
+                                encoding:NSUTF8StringEncoding
+                                   error:&error];
+  
+  if (succeeded) {
+    NSLog(@"Successfully stored the file at: %@",destinationPath);
+    MFMailComposeViewController *mailController = [[MFMailComposeViewController alloc] init];
+    [mailController setMailComposeDelegate:self];
+    [mailController setSubject:@"Doc File"];
+    NSString *textForEmail = @"";
+    [mailController setMessageBody:textForEmail isHTML:YES];
+    NSData *data = [[NSFileManager defaultManager] contentsAtPath:destinationPath];
+    if (data.length) {
+      [mailController addAttachmentData:data mimeType:@"file/doc" fileName:@"MyFile.doc"];
+    }
+    [self presentViewController:mailController animated:YES completion:^{
+      
+    }];
+  } else {
+    NSLog(@"Failed to store the file. Error = %@", error);
+  }
 }
 
 - (void)didReceiveMemoryWarning
